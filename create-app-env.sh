b210f3d90caf4b54a4800ffe059b5119813b0853
chandrika pai <chandrika pai>
2016-11-06 20:25:03 -0600

    added sns sqs and s3

--- create-app-env.sh
+++ create-app-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dbname="chandudb"
+dbname="Student"
 dbidentifier="itmo-chandudb"
 dbsgrp="sg-8f6fa4f6"
 dbinstance="db.t2.micro"
@@ -17,3 +17,16 @@ aws rds create-db-instance --db-name $dbname --db-instance-identifier $dbidentif
 aws rds wait db-instance-available --db-instance-identifier $dbidentifier
 
 echo "RDS instance successfully created"
+
+#creating SNS:
+savearn=`aws sns create-topic --name my-message`
+aws sns subscribe --topic-arn $savearn --protocol email --notification-endpoint cpai1@hawk.iit.edu
+
+#creating SQS:
+saveurl=`aws sqs create-queue --queue-name chanduQueue` 
+aws sqs send-message --queue-url $saveurl --message-body "Hello"
+aws sqs receive-message --queue-url $saveurl
+
+#creating s3 bucket:
+aws s3 mb s3://$1 --region us-west-2
+aws s3 mb s3://$2 --region us-west-2
