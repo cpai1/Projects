dace6e75a760b71677938acf5d824f6ecfd66cca
chandrika pai <chandrika pai>
2016-11-11 18:57:18 -0600

    made changes to the dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,68 +1,181 @@
 <?php
-require 'vendor/autoload.php';
 
+require 'vendor/autoload.php';
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
+'version' => 'latest',
 'region'  => 'us-west-2'
 ));
-
-
 
 $result = $client->describeDBInstances(array(
     'DBInstanceIdentifier' => 'itmo-chandudb',
 ));
 
-
-$endpoint = ""; 
-
-
-foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
-    // Do something with the message
-    echo "============". $ep . "================";
-    $endpoint = $ep;
-}
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+print_r($endpoint);
 
 echo "begin database";
-$link = mysqli_connect("AWS URL *.us-west-2.rds.amazonaws.com","chandudb","chandu123","School") or die("Error " . mysqli_error($link));
-
-/* check connection */
+$link = mysqli_connect($endpoint,"chandudb","chandu123","School",3306) or die("Error " . mysqli_error($link));
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
 
-/* Insert statement */
-$stmt =$db->stmt_init();
-$stmt->prepare("INSERT INTO student (name,age) VALUES(?, ?)");
-foreach($myarray as $row)
-{
-    $stmt->bind_param('idsb', $row['fld1'], $row['fld2'])
-    $stmt->execute();
+$create_table = 'CREATE TABLE IF NOT EXISTS Students
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INT(3) NOT NULL,
+    PRIMARY KEY(id)
+)';
+
+$create_tbl = $link->query($create_table);
+if ($create_table) {
+
+        echo "<b>Table created successfully</b>";
+
 }
-$stmt->close();
-
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+else {
+        echo "There is an error!!";
 }
 
-printf("%d Row inserted.\n", $stmt->affected_rows);
+#Insert data
 
+$sql = "INSERT INTO Students (name, age) VALUES ('Chan',26), ('karthik', 24), ('akhil',25), ('anusha', 26), ('adi',26)";
+if ($link->query($sql) === TRUE) {
+    echo "Record Inserted successfully";
 
-/* explicit close recommended */
-$stmt->close();
+} else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+}
 
-$link->real_query("SELECT * FROM student");
+#display data
+
+$link->real_query("SELECT * FROM Students");
 $res = $link->use_result();
-
-echo "Result set order...\n";
+   echo "<br/>";
+   echo "<table>";
+   echo "<tr>";
+   echo "<th> ID </th>";
+   echo "<th> Name </th>";
+   echo "<th> Age </th>";
+   echo "</tr>";
 while ($row = $res->fetch_assoc()) {
-    echo " id = " . $row['id'] . "\n";
+    echo "<tr>";
+    echo "<td>";
+    echo $row['id'];
+    echo "</td>";
+    echo "<td>";
+    echo $row['name'];
+    echo "</td>";
+    echo "<td>";
+    echo $row['age'];
+    echo "</td>";
+    echo "</tr>";
 }
 
 
 $link->close();
-
-
-
-
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
